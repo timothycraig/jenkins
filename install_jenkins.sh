9203c49c8d4a90225b8db0dbf46aba5d02568934
Timothy Craig <ttcraig@gmail.com>
2017-11-15 17:25:34 -0500

    initial commit

---
+++ install_jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+usermod -aG docker ubuntu
+
+# run jenkins
+mkdir -p /var/jenkins_home
+chown -R 1000:1000 /var/jenkins_home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins
+
+# show endpoint
+echo 'Jenkins installed'
+echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
